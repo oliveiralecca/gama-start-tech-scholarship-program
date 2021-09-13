714534a0dd36170b8c0ee6bdee3742ccd79c85d1
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2021-09-13 20:50:20 -0300

    tests module update

--- 08-testes/tdd-bdd-jasmine-cucumber/models/cliente.js
+++ 08-testes/tdd-bdd-jasmine-cucumber/models/cliente.js
@@ -1,8 +1,8 @@
 module.exports = class Cliente {
-  constructor() {
-    this.id = 0
-    this.nome = ""
-    this.telefone = ""
+  constructor(id=0, nome="", telefone="") {
+    this.id = id
+    this.nome = nome
+    this.telefone = telefone
   }
 
   nomeUppercase() {
@@ -15,16 +15,16 @@ module.exports = class Cliente {
 
   static todos() {
     return [
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
-      new Cliente(),
+      new Cliente(1, "Letícia 1", "786946467"),
+      new Cliente(2, "Letícia 2", "786988667"),
+      new Cliente(3, "Letícia 3", "786983127"),
+      new Cliente(4, "Letícia 4", "786983465"),
+      new Cliente(5, "Letícia 5", "786983448"),
+      new Cliente(6, "Letícia 6", "786983489"),
+      new Cliente(7, "Letícia 7", "776983467"),
+      new Cliente(8, "Letícia 8", "986983467"),
+      new Cliente(9, "Letícia 9", "787983467"),
+      new Cliente(10, "Letícia 10", "786983467"),
     ]
   }
 }
