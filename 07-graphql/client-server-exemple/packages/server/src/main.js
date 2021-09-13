926db2a45b33048aebfa60ca6ad5e3d44fd62ce9
Letícia Oliveira <oliveiraslc@yahoo.com.br>
2021-09-13 15:25:59 -0300

    pnpm update

--- 07-graphql/client-server-exemple/packages/server/src/main.js
+++ 07-graphql/client-server-exemple/packages/server/src/main.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+<<<<<<< Updated upstream
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
+=======
+>>>>>>> Stashed changes
 
 const app = express();
 
 const server = new ApolloServer({
+<<<<<<< Updated upstream
   typeDefs,
   resolvers,
 })
@@ -17,6 +21,34 @@ server.applyMiddleware({
   },
   bodyParserConfig: true,
 })
+=======
+
+})
+
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: 'http://localhost:3000',
+  }
+})
+
+// server.get('/status', (_, response) => {
+//   response.send({
+//     status: 'Everything OK'
+//   })
+// })
+
+// const enableCors = cors({ origin: 'http://localhost:3000' })
+
+// server
+//   .options('/authenticate', enableCors)
+//   .post('/authenticate', enableCors, express.json(), (request, response) => {
+//     console.log('E-mail', request.body.email, 'Senha', request.body.password);
+//     response.send({
+//       Ok: true,
+//     });
+//   })
+>>>>>>> Stashed changes
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
